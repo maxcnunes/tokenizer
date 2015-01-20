dd4ead0d4b15bbb4a496fd160ac6f06cbeba7fa7
Max Claus Nunes <maxcnunes@gmail.com>
2015-01-20 21:06:12 -0200

    adds support to read from configuration file

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # tokenizer
 
 Generates oauth2 tokens
+
+
+Configuration file **tokenizer.json**:
+
+```json
+{
+    "URL": "http://oauth2-service.example/authentication-route",
+    "GrantType": "client_credentials",
+    "ClientId": "1",
+    "ClientSecret": "xxxxxxxxxxxx"
+}
+```
--- main.go
+++ main.go
@@ -1,18 +1,42 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"os"
 )
 
+type Configuration struct {
+	URL          string
+	GrantType    string
+	ClientId     string
+	ClientSecret string
+}
+
 func main() {
+	file, err := os.Open("tokenizer.json")
+	if err != nil {
+		fmt.Println("Error reading config file", err)
+		return
+	}
+
+	decoder := json.NewDecoder(file)
+	config := Configuration{}
+
+	err = decoder.Decode(&config)
+	if err != nil {
+		fmt.Println("Error parsing config", err)
+		return
+	}
+
 	urlEncoded := url.Values{}
-	urlEncoded.Add("grant_type", "xxxxx")
-	urlEncoded.Add("client_id", "xxxxx")
-	urlEncoded.Add("client_secret", "xxxxx")
-	res, err := http.PostForm("http://xxxx", urlEncoded)
+	urlEncoded.Add("grant_type", config.GrantType)
+	urlEncoded.Add("client_id", config.ClientId)
+	urlEncoded.Add("client_secret", config.ClientSecret)
+	res, err := http.PostForm(config.URL, urlEncoded)
 
 	if err != nil {
 		fmt.Println("Error posting", err)
